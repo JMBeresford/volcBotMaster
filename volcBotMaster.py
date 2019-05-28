0b0b7567ae04b2a065540640578bf2850fa05206
John Beresford <jberesford@volcaus.com>
2019-05-27 21:43:00 -0700

    standard issue updates

--- volcBotMaster.py
+++ volcBotMaster.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 import logging
 import json
 
@@ -11,25 +12,49 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-client = discord.Client()
+client = commands.Bot(command_prefix = '?')
 
 @client.event
 async def on_ready():
     """Start-up procedure"""
-
     print('Successfully logged on to Discord as {0.user}'.format(client))
 
     for guild in client.guilds:
         print('\tAccessing server: ' + str(guild) + '\n')
 
-@client.event
-async def on_message(message):
-    if message.author is not client.user:
-        print('Message from {0.author}: {0.content}'.format(message))
 
-    if message.content == "die":
-        await message.channel.send(message.author.mention + ' no u die')
-        
+""" COMMANDS """
+
+# For fun commands
+@client.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+@client.command()
+async def foo(ctx):
+    await ctx.send('Bar!')
+
+@client.command()
+async def whisper(ctx, target : discord.Member, *, msg):
+    await target.send(ctx.message.author.name + ctx.message.author.discriminator + ' said: ' + msg)
+
+
+
+# Moderator Commands
+@client.command()
+async def purge(ctx, amount, target : discord.Member):
+    def targetAquired(msg):
+        return True if msg.author == target else False
+
+    messageMurder = await ctx.message.channel.purge(limit=100, check=targetAquired, before=ctx.message)
+
+    await ctx.send("Purged {} of {}'s messages.".format(len(messageMurder), target.name))
+
+@client.command()
+async def kick(ctx, target : discord.Member, *, because='because reasons'):
+    await target.kick(reason=because)
+
+    await ctx.message.channel.send('Get :clap: him :clap: outta :clap: here :clap:')
 
 
 client.run(token)
