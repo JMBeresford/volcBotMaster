78804409bd0d49842916a7e0b0736b716780c862
John Beresford <jberesford@volcaus.com>
2019-07-19 23:57:50 -0700

    added server growth graph

--- augments/Graphing.py
+++ augments/Graphing.py
@@ -4,7 +4,9 @@ import sqlite3 as sql
 import os
 import matplotlib.pyplot as plt
 import numpy as np
-from operator import itemgetter
+from dateutil.relativedelta import relativedelta
+from dateutil.rrule import rrule, MONTHLY, DAILY
+from datetime import datetime
 
 """The logic for graphing is all implemented here"""
 
@@ -20,30 +22,32 @@ class Graphing(commands.Cog):
         return 'User Not Found'
 
     @commands.command()
-    async def serveractivity(self, ctx, *, x='week'):
-        date = ctx.message.created_at
-        graph_values = {}
-
+    async def serveractivity(self, ctx, *, time='week'):  # TODO: make time interchangeable
         with sql.connect(f'data/{ctx.guild.id}/stats.db') as conn:
             cursor = conn.cursor()
             cursor.execute("SELECT datetime FROM activity WHERE DATE(datetime)"  # querying the db for datetime objects
-                           " BETWEEN DATE('now','-7 day') AND DATE('now')")
+                           " BETWEEN DATE('now','-7 day') AND DATE('now')")      # from the last 7 days
 
             dated_messages = [date_time for date_time in cursor]  # Sqlite returns list of tuples
-            dated_messages = [date.fromisoformat(tup[0])          # thus these hoops we jump through
+            dated_messages = [datetime.fromisoformat(tup[0])          # thus these hoops we jump through
                               for tup in dated_messages]
 
         conn.close()
 
-        for day in range(date.day-7, date.day):
-            message_list = [msg for msg in dated_messages if msg.day == day]
-            graph_values[day] = len(message_list)
+        before = datetime.today() - relativedelta(days=6)
+
+        dated_messages = [dt.strftime('%a %d') for dt in dated_messages]
+
+        x = [dt.strftime('%a %d') for dt in rrule(DAILY, count=7, dtstart=before)]
+        y = {}
+        for day in x:
+            y[day] = dated_messages.count(day)
+
+        y = list(y.values())
 
         fig, ax = plt.subplots()
-        x = list(graph_values.keys())
-        y = list(graph_values.values())
         ax.plot(x, y)
-        ax.set(xlabel="Day of Month", ylabel="Message Count",
+        ax.set(ylabel="Message Count",
                title="Server Activity")
         ax.grid()
         fig.savefig(f'data/{ctx.guild.id}/graph.png')
@@ -59,10 +63,10 @@ class Graphing(commands.Cog):
 
         conn = sql.connect(f'data/{guild}/stats.db')
         cursor = conn.cursor()
-        cursor.execute("SELECT id, message_count FROM members")
+        cursor.execute("SELECT id, message_count FROM members"
+                       " ORDER BY message_count DESC LIMIT 10")
 
-        data = sorted(cursor.fetchall(), key=itemgetter(1))  # sorts the list of tuples by the [1] index values
-        data.reverse()
+        data = cursor.fetchall()
         users = [await self.id_to_member(user[0], ctx) for user in data[0:10]]
         names = [user.name for user in users]
         msg_count = [user[1] for user in data[0:10]]
@@ -80,6 +84,7 @@ class Graphing(commands.Cog):
         ax.set_xticklabels(names, rotation=25, rotation_mode='anchor')
         ax.tick_params(axis='x', labelsize=9, pad=25)
 
+        # tight option because xtick labels get cut off otherwise, due to rotation
         fig.savefig(f'data/{guild}/graph.png', bbox_inches='tight')
 
         await ctx.send(content=f'The top chatters for the {ctx.guild} server:',
@@ -89,7 +94,43 @@ class Graphing(commands.Cog):
         if author not in users:
             await ctx.send(f"Step it up, {author.mention}, you're not even on there :smirk:")
 
+    @commands.command()
+    async def servergrowth(self, ctx, time='year'):  # TODO: make time interchangeable
+        date_of = datetime.now()
+        guild = ctx.guild.id
 
+        conn = sql.connect(f'data/{guild}/stats.db')
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT join_date FROM members"
+                       " WHERE DATE(join_date) BETWEEN DATE('now', '-12 month') AND DATE('now')"
+                       " ORDER BY join_date ASC")
+        data = cursor.fetchall()
+        conn.commit()
+        conn.close()
+        dates = [date_of.fromisoformat(dates[0]) for dates in data]
+        dates = [date_string.strftime('%b-%y') for date_string in dates]
+        before = date_of
+        before -= relativedelta(months=11)
+        x = [dt.strftime('%b-%y') for dt in rrule(MONTHLY, count=12, dtstart=before)]
+        y = {}
+        for month in x:
+            y[month] = dates.count(month)
+
+        y = list(y.values())
+
+        fig, ax = plt.subplots()
+        ax.grid()
+        ax.plot(x, y)
+        ax.tick_params(axis='x', labelsize=7)
+        ax.set(ylabel="Number of New Members",
+               title="Server Growth")
+
+        fig.savefig(f'data/{guild}/graph.png', bbox_inches='tight')
+
+        await ctx.send(content=f'The growth of the {ctx.guild} server:',
+                       file=File(f'data/{guild}/graph.png'))
+        os.remove(f'data/{guild}/graph.png')
 
 
 def setup(client):
--- augments/Nonsense.py
+++ augments/Nonsense.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 
 # TODO: favoriteWord, favoriteCommand
 
+
 class Nonsense(commands.Cog):
     def __init__(self, client):
         self.client = client
